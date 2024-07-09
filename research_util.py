5a600b940d543dc483c8014d70d870864fda5de0
keliankaz <kdascher@ucsc.edu>
2024-07-08 18:43:19 -0700

    option for lettering to be customizable by location on plot

--- research_util.py
+++ research_util.py
@@ -1,5 +1,11 @@
 import string
 
 def add_lettering(AX, position=[0.008, 0.925]):
-    for n, ax in enumerate(AX):
-        ax.text(*position, string.ascii_uppercase[n], transform=ax.transAxes, weight='bold')
+    if type(position[0]) is float:
+        position = len(AX) * [position]
+    else:
+        if len(position) != len(AX): 
+            raise ValueError
+    
+    for (n, ax), pos in zip(enumerate(AX), position):
+        ax.text(*pos, string.ascii_uppercase[n], transform=ax.transAxes, weight='bold')
